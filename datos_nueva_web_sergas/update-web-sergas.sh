4d11f6cfa192197e6f0baa9beb1953632a407f27
lipido <dgpena@uvigo.es>
2020-10-13 11:57:42 +0200

    Añade información cruda de la nueva Web del SERGAS a 13/10/2020

--- datos_nueva_web_sergas/update-web-sergas.sh
+++ datos_nueva_web_sergas/update-web-sergas.sh
@@ -14,39 +14,39 @@ fi
 DATA_DATE=$(date --date "$PUBDATE -1 days" "+%Y-%m-%d")
 
 if [ -z $NOAPPEND ]; then
-  wget -O - "https://coronavirus.sergas.gal/infodatos/${DATA_DATE}_COVID19_Web_CifrasTotais.csv" | tail -n +2 | tr -d '\r' >> CifrasTotais.csv 
+  wget -O - "https://coronavirus.sergas.gal/infodatos/${DATA_DATE}_COVID19_Web_CifrasTotais.csv" | tail -n +2  | tr -d '\r' | grep -v "^$" >> CifrasTotais.csv 
 else
-  wget -O - "https://coronavirus.sergas.gal/infodatos/${DATA_DATE}_COVID19_Web_CifrasTotais.csv" | tr -d '\r' > CifrasTotais.csv 
+  wget -O - "https://coronavirus.sergas.gal/infodatos/${DATA_DATE}_COVID19_Web_CifrasTotais.csv" | tr -d '\r' | grep -v "^$" > CifrasTotais.csv 
 fi
 
 if [ -z $NOAPPEND ]; then
-  wget -O - "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_OcupacionCamasHospital.csv" | tail -n +2 | tr -d '\r' >> OcupacionCamasHospital.csv
+  wget -O - "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_OcupacionCamasHospital.csv" | tail -n +2 | tr -d '\r' | grep -v "^$" >> OcupacionCamasHospital.csv
 else
-  wget -O - "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_OcupacionCamasHospital.csv" | tr -d '\r' > OcupacionCamasHospital.csv
+  wget -O - "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_OcupacionCamasHospital.csv" | tr -d '\r' | grep -v "^$" > OcupacionCamasHospital.csv
 fi
 
 # serie
-wget -O - "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_ActivosCuradosFallecidos.csv" | tr -d '\r' > ActivosCuradosFallecidos.csv
+wget -O - "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_ActivosCuradosFallecidos.csv" | tr -d '\r' | grep -v "^$" > ActivosCuradosFallecidos.csv
 
 
 # serie
-wget -O - "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_InfectadosPorFecha.csv" | tr -d '\r' > InfectadosPorFecha.csv
+wget -O - "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_InfectadosPorFecha.csv" | tr -d '\r' | grep -v "^$" > InfectadosPorFecha.csv
 
 
 # acumulado
 if [ -z $NOAPPEND ]; then
-wget -O -  "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_Fallecidos.csv" | tail -n +2 | tr -d '\r' | awk -v fecha="\"${DATA_DATE}\"" -e 'BEGIN{OFS=","; FS=","; FPAT = "([^,]+)|(\"[^\"]+\")"; _fecha=fecha}{print _fecha,$1,$2,$3,$4}' >> Fallecidos.csv
+wget -O -  "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_Fallecidos.csv" | tail -n +2 | tr -d '\r' | grep -v "^$" | awk -v fecha="\"${DATA_DATE}\"" -e 'BEGIN{OFS=","; FS=","; FPAT = "([^,]+)|(\"[^\"]+\")"; _fecha=fecha}{print _fecha,$1,$2,$3,$4}' >> Fallecidos.csv
 else
-wget -O -  "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_Fallecidos.csv" | tr -d '\r' | awk -v fecha="\"${DATA_DATE}\"" -e 'BEGIN{OFS=","; FS=","; FPAT = "([^,]+)|(\"[^\"]+\")"; _fecha=fecha}{if (NR==1) {_fecha="Fecha"} else {_fecha=fecha} print _fecha,$1,$2,$3,$4}' > Fallecidos.csv
+wget -O -  "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_Fallecidos.csv" | tr -d '\r' | grep -v "^$" | awk -v fecha="\"${DATA_DATE}\"" -e 'BEGIN{OFS=","; FS=","; FPAT = "([^,]+)|(\"[^\"]+\")"; _fecha=fecha}{if (NR==1) {_fecha="Fecha"} else {_fecha=fecha} print _fecha,$1,$2,$3,$4}' > Fallecidos.csv
 fi
 
 # serie
-wget -O - "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_PorcentajeInfeccionesPorFecha.csv" | tr -d '\r' > PorcentajeInfeccionesPorFecha.csv
+wget -O - "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_PorcentajeInfeccionesPorFecha.csv" | tr -d '\r' | grep -v "^$" > PorcentajeInfeccionesPorFecha.csv
 
 
 # acumulado 
 if [ -z $NOAPPEND ]; then
-wget -O -  "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_PorcentajeInfectadosPorGenero.csv" |tail -n +2 | tr -d '\r' | awk -v fecha="\"${DATA_DATE}\"" -e 'BEGIN{OFS=","; FS=","; FPAT = "([^,]+)|(\"[^\"]+\")"; _fecha=fecha}{print _fecha,$1,$2,$3,$4}' >> PorcentajeInfectadosPorGenero.csv
+wget -O -  "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_PorcentajeInfectadosPorGenero.csv" |tail -n +2 | tr -d '\r' | grep -v "^$" | awk -v fecha="\"${DATA_DATE}\"" -e 'BEGIN{OFS=","; FS=","; FPAT = "([^,]+)|(\"[^\"]+\")"; _fecha=fecha}{print _fecha,$1,$2,$3,$4}' >> PorcentajeInfectadosPorGenero.csv
 else
-wget -O -  "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_PorcentajeInfectadosPorGenero.csv" | tr -d '\r' | awk -v fecha="\"${DATA_DATE}\"" -e 'BEGIN{OFS=","; FS=","; FPAT = "([^,]+)|(\"[^\"]+\")"; _fecha=fecha}{if (NR==1) {_fecha="Fecha"} else {_fecha=fecha} print _fecha,$1,$2,$3,$4}' > PorcentajeInfectadosPorGenero.csv
+wget -O -  "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_PorcentajeInfectadosPorGenero.csv" | tr -d '\r' | grep -v "^$" | awk -v fecha="\"${DATA_DATE}\"" -e 'BEGIN{OFS=","; FS=","; FPAT = "([^,]+)|(\"[^\"]+\")"; _fecha=fecha}{if (NR==1) {_fecha="Fecha"} else {_fecha=fecha} print _fecha,$1,$2,$3,$4}' > PorcentajeInfectadosPorGenero.csv
 fi
