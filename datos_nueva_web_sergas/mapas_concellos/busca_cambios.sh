b4ff0ff5ae1d4956e083debabb6979a2f609a57b
lipido <dgpena@uvigo.es>
2020-10-14 11:47:30 +0200

    Corrige script de búsqueda de cambios
    El script fallaba cuando dos ayuntamientos contiguos en el listado
    habían cambiado. Esta nueva versión emplea otra estrategia que
    es más robusta.

--- datos_nueva_web_sergas/mapas_concellos/busca_cambios.sh
+++ datos_nueva_web_sergas/mapas_concellos/busca_cambios.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-diff $1 $2 | awk -e 'BEGIN{FS=","}{if (NR%2==0){ if(NR%4==0)print $4; else print}}' | sed -e 's/[<>] //g' | cut -d ',' -f 3,4 | awk '{if(NR%2!=0){printf "%s ",$0;next;}}1' | sed -e 's/\(.*\)Nivel de alerta \([0-9]\).*Nivel de alerta \([0-9]\).*/\1 \2 -> \3/g'
+diff -y $1 $2 | grep "|" | cut -d ',' -f 3,4,7  | tr -d '"' | sed -e 's/\(.*\)Nivel de alerta \([0-9]\).*Nivel de alerta \([0-9]\).*/\1 \2 → \3/g'
 
