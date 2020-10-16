2157d4448d590b1f187069ce4f22bf49efa18257
lipido <dgpena@uvigo.es>
2020-10-16 10:12:54 +0200

    Mejora el script para hacerlo más robusto

--- datos_nueva_web_sergas/mapas_concellos/busca_cambios.sh
+++ datos_nueva_web_sergas/mapas_concellos/busca_cambios.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-diff -y $1 $2 | grep "|" | cut -d ',' -f 3,4,7  | tr -d '"' | sed -e 's/\(.*\)Nivel de alerta \([0-9]\).*Nivel de alerta \([0-9]\).*/\1 \2 → \3/g'
-
+diff -y $1 $2 | grep "|" | cut -d ',' -f 3,2,5 | tr -d '"'| awk -e 'BEGIN{FS=","}{print $2,$1"→"$3}'
