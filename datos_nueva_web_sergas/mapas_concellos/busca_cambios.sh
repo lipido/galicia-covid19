820b908a89693a81cc187c540a11dde9d32de6cd
lipido <dgpena@uvigo.es>
2020-10-19 12:41:24 +0200

    Añade emojies para diferenciar los que suben de los que bajan
    Gracias a @cmcarquitecto por la idea

--- datos_nueva_web_sergas/mapas_concellos/busca_cambios.sh
+++ datos_nueva_web_sergas/mapas_concellos/busca_cambios.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-diff -y $1 $2 | grep "|" | cut -d ',' -f 3,2,5 | tr -d '"'| awk -e 'BEGIN{FS=","}{print $2,$1"→"$3}'
+diff -y $1 $2 | grep "|" | cut -d ',' -f 3,2,5 | tr -d '"'| awk -e 'BEGIN{FS=","}{if ($1 > $3) print  "📉", $2,$1"→"$3; else print  "📈", $2,$1"→"$3;}'
