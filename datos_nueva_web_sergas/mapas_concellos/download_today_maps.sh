72e6fbf403991316414e5df7f468961f56dd8010
lipido <dgpena@uvigo.es>
2020-11-11 12:15:04 +0100

    Corrige la descarga de mapas del SERGAS

--- datos_nueva_web_sergas/mapas_concellos/download_today_maps.sh
+++ datos_nueva_web_sergas/mapas_concellos/download_today_maps.sh
@@ -11,11 +11,11 @@ wget -O /tmp/mapa-covid-7.html https://datawrapper.dwcdn.net/${MAP_7_ID}/10/
 wget -O /tmp/mapa-covid-14.html https://datawrapper.dwcdn.net/${MAP_2_ID}/10/
 
 for i in 7 14; do
-  if [ "$(grep "REFRESH" /tmp/mapa-covid-${i}.html | wc -l)" -ne "0" ]; then
+  while [ "$(grep "REFRESH" /tmp/mapa-covid-${i}.html | wc -l)" -ne "0" ]; do
     new_url=$(cat /tmp/mapa-covid-${i}.html | sed -e 's/.*url=\(.*\)".*/\1/g' | tr -d '\r' | tr -d '\n')
     echo "following to: ${new_url}"
     wget -O /tmp/mapa-covid-${i}.html "${new_url}"
-  fi
+  done
 done
 
 
