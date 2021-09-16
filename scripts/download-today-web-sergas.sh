99f22b837025aa7acdc68e057921633efe2265eb
lipido <dgpena@uvigo.es>
2021-09-16 18:28:04 +0200

    Actualiza datos a 16/09/2021
    - Actualiza los ficheros de galicia.csv y de las 7 áreas sanitarias.
    
    - Añade información cruda de la nueva Web del SERGAS
    
    - Actualiza incidencia por municipios, actualizando tanto el mapa
    crudo del SERGAS como el csv con la incidencia.

---
+++ scripts/download-today-web-sergas.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+#fecha de publicación de los datos (por defecto será la de invocación del script)
+PUBDATE=$1
+
+if [ -z $PUBDATE ]; then
+  PUBDATE=$(date +"%Y-%m-%d")
+fi
+
+DATA_DATE=$(date --date "$PUBDATE -1 days" "+%Y-%m-%d")
+
+wget -O /tmp/datos_sergas.csv "https://coronavirus.sergas.gal/infodatos/${DATA_DATE}_COVID19_Web_CifrasTotais_PDIA.csv"
+
+wget -O /tmp/hospitales.csv "https://coronavirus.sergas.es/infodatos/${DATA_DATE}_COVID19_Web_OcupacionCamasHospital.csv"
+
+declare -A areas
+areas["ourense"]="OURENSE"
+areas["coruna"]="CORU"
+areas["ferrol"]="FERROL"
+areas["santiago"]="SANTIAGO"
+areas["lugo"]="LUGO"
+areas["pontevedra"]="PONTEVEDRA"
+areas["vigo"]="VIGO"
+areas["galicia"]="GALICIA"
+
+declare -A casos
+declare -A altas
+declare -A fallecidos
+declare -A pcr
+declare -A no_pdia
+declare -A antigeno
+declare -A confirmados_pcr_24h
+declare -A planta
+declare -A uci
+declare -A hospitalizados
+declare -A activos
+declare -A domicilio
+
+
+for FICHERO_AREA in "${!areas[@]}"; do
+  # Print the KEY value
+  
+  # Print the VALUE attached to that KEY
+  linea_area=$(cat /tmp/datos_sergas.csv | grep ${areas[$FICHERO_AREA]})
+  
+  variables=(casos confirmados_pcr_24h altas fallecidos pcr planta uci activos no_pdia antigeno)
+  columnas=(3 4 6 11 9 7 8 5 10 12)
+  for ((i = 0; i < ${#variables[@]}; ++i)); do
+    # bash arrays are 0-indexed
+    variable=${variables[$i]}
+    valor=$(echo $linea_area |  awk -v col=${columnas[$i]} -e 'BEGIN{FS=","; FPAT = "([^,]+)|(\"[^\"]+\")"}{print $col}' | tr -d "\"" | tr -d "."| tr -d "\r")
+    eval "${variable}[$FICHERO_AREA]=$valor"
+    
+   # echo ${array["hola"]}
+  done
+  
+done
+
+for FICHERO_AREA in "${!areas[@]}"; do
+  hospitalizados[$FICHERO_AREA]=$((${planta[$FICHERO_AREA]} + ${uci[$FICHERO_AREA]}))
+  domicilio[$FICHERO_AREA]=$((${activos[$FICHERO_AREA]} - ${hospitalizados[$FICHERO_AREA]}))
+done
+
+# hospitales Ourense
+
+declare -A nombres_hospitales_ourense
+declare -A hospitales_ourense_planta
+declare -A hospitales_ourense_uci
+declare -A hospitales_ourense_hospitalizados
+nombres_hospitales_ourense["chuo"]="C.H.U"
+nombres_hospitales_ourense["verin"]="VERIN"
+nombres_hospitales_ourense["barco"]="VALDEORRAS"
+nombres_hospitales_ourense["cosaga"]="TERESA"
+nombres_hospitales_ourense["carmen"]="CARMEN"
+
+for hospital_ourense in "${!nombres_hospitales_ourense[@]}"; do
+  hospitales_ourense_planta[$hospital_ourense]=$(cat /tmp/hospitales.csv | grep "OURENSE" | grep ${nombres_hospitales_ourense[$hospital_ourense]} |  awk  -e 'BEGIN{FS=","; FPAT = "([^,]+)|(\"[^\"]+\")"}{print $5}' | tr -d "\"" | tr -d "." | tr -d "\r")
+  hospitales_ourense_uci[$hospital_ourense]=$(cat /tmp/hospitales.csv | grep "OURENSE" | grep ${nombres_hospitales_ourense[$hospital_ourense]} |  awk -e 'BEGIN{FS=","; FPAT = "([^,]+)|(\"[^\"]+\")"}{print $6}' | tr -d "\"" | tr -d "\r")
+  uci2=${hospitales_ourense_uci[$hospital_ourense]}
+  planta=${hospitales_ourense_planta[$hospital_ourense]}
+  hospitales_ourense_hospitalizados[$hospital_ourense]=$(($uci2 + $planta))
+done 
+
+#echo ${domicilio["galicia"]}
+
+
+# Imprimir linea google drive
+
+function imprimeArea {
+  echo -ne ${casos[$area]}'\t'
+  echo -ne ${altas[$area]}'\t'
+  echo -ne ${fallecidos[$area]}'\t'
+  echo -ne ${activos[$area]}'\t'
+  echo -ne ${confirmados_pcr_24h[$area]}'\t'
+  echo -ne ${domicilio[$area]}'\t'
+  echo -ne ${hospitalizados[$area]}'\t'
+  echo -ne ${uci[$area]}'\t'
+  echo -ne ${pcr[$area]}'\t'
+  echo -ne '\t'${antigeno[$area]}'\t'
+  echo -ne ${no_pdia[$area]}
+}
+
+# Galicia
+echo -ne $PUBDATE'\t'
+echo -ne ${casos["galicia"]}'\t'
+echo -ne ${altas["galicia"]}'\t'
+echo -ne ${fallecidos["galicia"]}'\t'
+echo -ne ${activos["galicia"]}'\t'
+echo -ne ${confirmados_pcr_24h["galicia"]}
+
+for area in coruna vigo santiago pontevedra; do
+  echo -ne '\t'
+  imprimeArea
+done
+
+area="ourense"
+# Ourense
+echo -ne '\t'
+echo -ne ${casos[$area]}'\t'
+echo -ne ${altas[$area]}'\t'
+echo -ne ${fallecidos[$area]}'\t'
+echo -ne ${activos[$area]}'\t'
+echo -ne ${confirmados_pcr_24h[$area]}'\t'
+echo -ne ${domicilio[$area]}'\t'
+echo -ne ${hospitales_ourense_hospitalizados["chuo"]}'\t'
+echo -ne ${hospitales_ourense_hospitalizados["carmen"]}'\t'
+echo -ne ${hospitales_ourense_hospitalizados["cosaga"]}'\t'
+echo -ne ${hospitales_ourense_hospitalizados["barco"]}'\t'
+echo -ne ${hospitales_ourense_hospitalizados["verin"]}'\t'
+echo -ne ${hospitales_ourense_uci["chuo"]}'\t'
+echo -ne ${hospitales_ourense_uci["cosaga"]}'\t'
+echo -ne ${hospitales_ourense_uci["carmen"]}'\t'
+echo -ne ${pcr[$area]}'\t'
+echo -ne '\t'${antigeno[$area]}'\t'
+echo -ne ${no_pdia[$area]}
+
+for area in lugo ferrol; do
+  echo -ne '\t'
+  imprimeArea
+done
+echo -ne '\n'
---
+++ scripts/update-from-web-sergas.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+today_data=$(./download-today-web-sergas.sh)
+OUTPUT_DIR=../
+
+
+#Galicia
+echo "$today_data" | cut -d $'\t' -f 1-4,6 | tr '\t' ',' >> $OUTPUT_DIR/galicia.csv
+
+#Coruña
+echo "$today_data" | cut -d $'\t' -f 1,7-9,11  | tr '\t' ',' >> $OUTPUT_DIR/coruna.csv
+echo "$today_data" | cut -d $'\t' -f 1,12-18 | tr '\t' ',' >> $OUTPUT_DIR/coruna.ext.csv
+
+#Vigo
+echo "$today_data" | cut -d $'\t' -f 1,19-21,23 | tr '\t' ',' >> $OUTPUT_DIR/vigo.csv
+echo "$today_data" | cut -d $'\t' -f 1,24-30 | tr '\t' ',' >> $OUTPUT_DIR/vigo.ext.csv
+
+#Santiago
+echo "$today_data" | cut -d $'\t' -f 1,31-33,35 | tr '\t' ',' >> $OUTPUT_DIR/santiago.csv
+echo "$today_data" | cut -d $'\t' -f 1,36-42 | tr '\t' ',' >> $OUTPUT_DIR/santiago.ext.csv
+
+#Pontevedra
+echo "$today_data" | cut -d $'\t' -f 1,43-45,47 | tr '\t' ',' >> $OUTPUT_DIR/pontevedra.csv
+echo "$today_data" | cut -d $'\t' -f 1,48-54 | tr '\t' ',' >> $OUTPUT_DIR/pontevedra.ext.csv
+
+#Ourense
+echo "$today_data" | cut -d $'\t' -f 1,55-57,59 | tr '\t' ',' >> $OUTPUT_DIR/ourense.csv
+echo "$today_data" | cut -d $'\t' -f 1,60-72 | tr '\t' ',' >> $OUTPUT_DIR/ourense.ext.csv
+
+#Lugo
+echo "$today_data" | cut -d $'\t' -f 1,73-75,77 | tr '\t' ',' >> $OUTPUT_DIR/lugo.csv
+echo "$today_data" | cut -d $'\t' -f 1,78-84 | tr '\t' ',' >> $OUTPUT_DIR/lugo.ext.csv
+
+#Ferrol
+echo "$today_data" | cut -d $'\t' -f 1,85-87,89 | tr '\t' ',' >> $OUTPUT_DIR/ferrol.csv
+echo "$today_data" | cut -d $'\t' -f 1,90-96 | tr -d '\r' | tr '\t' ',' >> $OUTPUT_DIR/ferrol.ext.csv
+
